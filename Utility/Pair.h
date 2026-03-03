83110cd727b7293cbceeef85557b0b982c323b7a
GH050800 <grno@outlook.com>
2026-03-03 13:02:52 +0400

    fix

--- Utility/Pair.h
+++ Utility/Pair.h
@@ -4,14 +4,35 @@
 template <typename T1, typename T2>
 class Pair
 {
-private:
+public:
 	T1 first;
 	T2 second;
 
-public:
-	Pair(T1 firstValue, T2 secondValue) : first(firstValue), second(secondValue) {}
+	Pair() = default;
+
+	Pair(const T1& firstValue, const T2& secondValue) : first(firstValue), second(secondValue) {}
+
+	Pair(const Pair& other) : first(other.first), second(other.second) {}
+
+	Pair& operator=(const Pair& other)
+	{
+		if (this != &other)
+		{
+			first = other.first;
+			second = other.second;
+		}
+		return *this;
+	}
+
+	~Pair() = default;
+
 	T1 getFirst() const { return first; }
 	T2 getSecond() const { return second; }
+
+	bool operator < (const Pair& other) const { return first < other.first; };
+	bool operator > (const Pair& other) const { return first > other.first; };
+	bool operator == (const Pair& other) const { return first == other.first; };
+	bool operator != (const Pair& other) const { return first != other.first; };
 };
 
 #endif // PAIR_H
